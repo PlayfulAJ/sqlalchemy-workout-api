f72abe4ea12f4cbb362d32f79810c6cb5d3b2766
PlayfulAJ <jabaringure@gmail.com>
2026-07-27 17:10:53 +0300

    Fix workouts endpoint and database setup

--- server/app.py
+++ server/app.py
@@ -5,7 +5,23 @@ from models import *
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+@app.route('/workouts', methods=['GET'])
+def get_workouts():
+    workouts = Workout.query.all()
+
+    workout_list = []
+
+    for workout in workouts:
+        workout_list.append({
+            "id": workout.id,
+            "date": str(workout.date),
+            "duration_minutes": workout.duration_minutes,
+            "notes": workout.notes
+        })
+
+    return make_response(workout_list, 200)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/jabar/Moringa/sqlachemy-workout-api/server/instance/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
